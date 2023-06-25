06bd876cbd168b31eb14da97e2eaf370e065db25
vagisha04 <100015459+vagisha04@users.noreply.github.com>
2023-06-25 15:38:02 +0530

    Time: 20 ms (20.93%), Space: 10.2 MB (54.81%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,63 +1,69 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
-        int m = board.size(), n = board[0].size();
-        vector<vector<int>> visited(m, vector<int>(n));
-
-        vector<vector<char>> ans(m, vector<char>(n, 'X'));
-        stack<pair<int, int>> st;
-
-        for (int j = 0; j < n; ++j)
+     void dfs(int row,int col, vector<vector<int>> &visited,vector<vector<char>>& board)
+    {
+          int m = board.size();
+        int n = board[0].size();
+        visited[row][col]=1;
+         int drow[]={-1,0,+1,0};
+        int dcol[]={0,+1,0,-1};
+        for(int i =0;i<4;i++)
         {
-            if (board[0][j] == 'O') {
-                visited[0][j] = 1;
-                st.push({0, j});
-            } 
-            if (board.size() > 1) {
-                if (board[m-1][j] == 'O'){
-                    visited[m-1][j] = 1;
-                    st.push({m-1, j});
-                }
-            }
-        }
-
-        for (int i = 1; i < m-1; ++i)
-        {
-            if (board[i][0] == 'O') {
-                visited[i][0] = 1;
-                st.push({i, 0});
-            } 
-            if (board.size() > 1) {
-                if (board[i][n-1] == 'O'){
-                    visited[i][n-1] = 1;
-                    st.push({i, n-1});
-                }
-            }
-        }
-        
-        while(st.size()){
-            pair<int, int> p = st.top();
-            st.pop();
-
-            int r = p.first, c = p.second, nRow, nCol;
-            ans[r][c] = 'O';
-
-            vector<int> rowShift = {-1, 0, +1, 0}, colShift = {0, -1, 0, +1};
-
-            for (int i = 0; i < 4; ++i)
+            int nrow = row+drow[i];
+            int ncol = col+dcol[i];
+            if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && visited[nrow][ncol]==0 && board[nrow][ncol]=='O')
             {
-                nRow = r + rowShift[i];
-                nCol = c + colShift[i];
-
-                if (nRow >= 0 && nCol >= 0 && nRow < m && nCol < n){
-                    if (board[nRow][nCol] == 'O' && !visited[nRow][nCol]){
-                        st.push({nRow, nCol});
-                        visited[nRow][nCol] = 1;
-                    }
-                }
-            }             
-        }
-
-        board = ans;
+                dfs(nrow,ncol,visited,board);
+}
+}
+}
+    void solve(vector<vector<char>>& board) {
+         int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>> visited(m,vector<int>(n,0));
+       
+        for(int i =0;i<m;i++)
+        {
+for(int j=0;j<n;j++)
+{
+    visited[i][j]=0;
+  
+}}
+        for(int i=0;i<m;i++)
+        {
+            if(board[i][0]=='O' && visited[i][0]==0)
+            {
+                dfs(i,0,visited,board);
+}
+            if(board[i][n-1]=='O' && visited[i][n-1]==0)
+            {
+                dfs(i,n-1,visited,board);
+}
+}
+         for(int i=0;i<n;i++)
+        {
+            if(board[0][i]=='O' && visited[0][i]==0)
+            {
+                dfs(0,i,visited,board);
+}
+            if(board[m-1][i]== 'O' && visited[m-1][i]==0)
+            {
+                dfs(m-1,i,visited,board);
+}
+}
+        
+         for(int i =0;i<m;i++)
+        {
+for(int j=0;j<n;j++)
+{
+   
+    if(visited[i][j]==0 && board[i][j]=='O')
+    {
+       board[i][j]='X';
+}}
+         }
+      
+        
+        
     }
 };
